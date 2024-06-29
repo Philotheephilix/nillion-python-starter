a3746e41bace3e0a2bf75487f0016a3f7a1b22d5
Philotheephilix <philosanjay5@gmail.com>
2024-06-29 17:59:41 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,38 @@
 from nada_dsl import *
+import random
 
+def generate_random_name(length):
+    name = []
+    for _ in range(length):
+        random_char = random.randint(97, 122)  # Random lowercase ASCII character (a-z)
+        name.append(random_char)
+    return "".join(chr(char) for char in name)
 
 def nada_main():
     party1 = Party(name="Party1")
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
+    # Generate random names (5 characters each)
+    person1_name = generate_random_name(5)
+    person2_name = generate_random_name(5)
 
-    return [Output(my_int1, "my_output", party1)]
+    # Calculate the love index using the secret integers
+    love_index = my_int1+my_int2
+
+    # Get the first letter of each person's name
+    person1_initial = person1_name[0]
+    person2_initial = person2_name[0]
+
+    # Create the output message
+    love_message = f"{person1_name} ({person1_initial}) ❤️ {person2_name} ({person2_initial}):"
+
+    return [Output(love_index, love_message, party1)]
+
+
+
+
+
+
+
+
